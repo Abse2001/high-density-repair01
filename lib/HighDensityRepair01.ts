650797c8bacc195ba9dbf1706eb0401f0b3b2f5d
seveibar <seveibar@gmail.com>
2026-03-21 16:41:03 -0700

    format, link to visualize

--- lib/HighDensityRepair01.ts
+++ lib/HighDensityRepair01.ts
@@ -1,19 +1,20 @@
-import { BaseSolver } from "@tscircuit/solver-utils";
-import type { GraphicsObject } from "graphics-debug";
-import type { HighDensityRepair01Input } from "./types";
+import { BaseSolver } from "@tscircuit/solver-utils"
+import type { GraphicsObject } from "graphics-debug"
+import type { HighDensityRepair01Input } from "./types"
+import { visualizeHighDensityRepair } from "./visualizeHighDensityRepair"
 
 export class HighDensityRepair01 extends BaseSolver {
-	constructor(public inputParams: HighDensityRepair01Input) {
-		super();
-	}
+  constructor(public inputParams: HighDensityRepair01Input) {
+    super()
+  }
 
-	override getConstructorParams(): HighDensityRepair01Input | undefined {
-		return this.inputParams;
-	}
+  override getConstructorParams(): HighDensityRepair01Input | undefined {
+    return this.inputParams
+  }
 
-	override _step() {}
+  override _step() {}
 
-	override visualize(): GraphicsObject {
-		return {} as GraphicsObject;
-	}
+  override visualize(): GraphicsObject {
+    return visualizeHighDensityRepair(this)
+  }
 }
